66927ffa8861d4badfb5fec59261ad597058da92
のぶ <nobuhiko.kimoto@gmail.com>
2014-03-07 13:14:41 +0900

    プロンプトを修正

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-mv $HOME/.bashrc $HOME/bashrcbk
-mv $HOME/.vimrc $HOME/vimrcbk
-mv $HOME/.gitconfig $HOME/gitconfigbk
-mv $HOME/.gitignore $HOME/gitignoregbk
-ln -s $HOME/dotfiles/_bashrc $HOME/.bashrc
-ln -s $HOME/dotfiles/_git-completion.bash $HOME/.git-completion.bash
-ln -s $HOME/dotfiles/_vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/vimfiles $HOME/.vim
-ln -s $HOME/dotfiles/_gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/_gitignore $HOME/.gitignore
+ln -sb $HOME/dotfiles/_bashrc $HOME/.bashrc
+ln -sb $HOME/dotfiles/_vimrc $HOME/.vimrc
+ln -sb $HOME/dotfiles/vimfiles $HOME/.vim
+ln -sb $HOME/dotfiles/_gitconfig $HOME/.gitconfig
+ln -sb $HOME/dotfiles/_gitignore $HOME/.gitignore
 source $HOME/.bashrc
